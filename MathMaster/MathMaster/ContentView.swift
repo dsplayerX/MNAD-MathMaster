1e2bd9d6f7a33507b3da04015b578e430582d5ab
dsplayerX <dumindu0043@gmail.com>
2024-10-09 17:00:46 +0530

    Initial commit

---
+++ MathMaster/MathMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathMaster
+//
+//  Created by Dumindu Sameendra on 2024-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathMaster/MathMaster/MathMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathMasterApp.swift
+//  MathMaster
+//
+//  Created by Dumindu Sameendra on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct MathMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
